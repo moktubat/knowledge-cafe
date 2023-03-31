1912a394a4fa7ddf895b0d403de9d531b6c4d519
moktubat <shadhinreja@gmail.com>
2023-04-01 01:40:27 +0600

    blog q and a added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -20,6 +20,12 @@ const Blog = () => {
                 <legend><h2>3. Purpose of useEffect other than fetching data.</h2></legend>
                 <p><code>useEffect</code> used for Fetching data, Reading from local storage, Registering and de-registering event listeners. For clean up mount action like removing event listeners or something data fetching we used useEffect component unmounts.</p>
             </fieldset>
+            <fieldset>
+                <legend><h2>4. How Does React work?</h2></legend>
+                <p>
+                React is a JavaScript library to use in web development for build a interactive elements websites. But if you’re not familiar with JavaScript or JavaScript libraries, that’s not a helpful for you. React. ReactJS divides the UI into various reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. 
+                </p>
+            </fieldset>
         </div>
     );
 };
